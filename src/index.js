64cc07f3a85a5b28ad8c6f36e769fb47532b6b31
evgenynovak <evnovak171@mail.ru>
2019-08-24 18:47:08 +0300

    project preparation: cleaning, changing workpieces

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Article() {
+  const body = <section>Footer</section> 
+ return(
+      <div className="hello" style={{color:"red"}}>
+        <h2>title</h2>
+        <section>body</section> 
+        { body }
+        <h3 style={{color:"red"}}>
+         "creation date : "{ (new Date()).toDateString()}
+        </h3>
+      </div>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+ return (
+    <div>
+        <h1>App Name</h1>
+        <Article/>
+    </div>
+  );
+}
+
+render(<App/> ,document.getElementById('root'))
